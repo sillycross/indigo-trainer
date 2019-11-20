246ea10a05e74f5f3ea37d90d35e09fdc8a9f2cc
Haoran Xu <haoranxu510@gmail.com>
2019-11-20 10:42:52 -0800

    zzzz

--- leaf_setup.py
+++ leaf_setup.py
@@ -12,6 +12,9 @@ from env_vars import *
 
 os.chdir(PROJECT_ROOT)
 
+exit_code = os.system('sudo apt-get install -y libz-dev')
+assert(exit_code == 0)
+
 exit_code = os.system('git clone https://github.com/%s/%s' % (GITHUB_USER_NAME, MODEL_REPO_NAME))
 assert(exit_code == 0)
 
@@ -23,7 +26,7 @@ os.chdir(PROJECT_ROOT + '/' + LKM_REPO_NAME + '/training_output_formatter')
 exit_code = os.system('make -B')
 assert(exit_code == 0)
 
-exit_code = os.system('cp training_output_formatter ..')
+exit_code = os.system('cp training_output_formatter ../..')
 assert(exit_code == 0)
 
 print('Leaf setup complete.')
