1de31cb28030dbadb39f5d055b972e3abb4386e5
Haoran Xu <haoranxu510@gmail.com>
2019-11-20 20:11:44 -0800

    sda

--- logger.py
+++ logger.py
@@ -2,12 +2,14 @@ import logging
 
 from env_vars import *
 
+logging.basicConfig(level=logging.DEBUG)
+
 logger = logging.getLogger('')
 
 c_handler = logging.StreamHandler()
 f_handler = logging.FileHandler(PROJECT_ROOT + '/indigo.log')
-c_handler.setLevel(logging.INFO)
-f_handler.setLevel(logging.INFO)
+c_handler.setLevel(logging.DEBUG)
+f_handler.setLevel(logging.DEBUG)
 
 c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
